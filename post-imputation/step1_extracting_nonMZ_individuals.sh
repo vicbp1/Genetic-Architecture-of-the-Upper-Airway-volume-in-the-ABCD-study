2eac37a488adf84c4cf619a07f6fded3dae21745
Victor Borda <vicbp1@gmail.com>
2025-06-26 12:13:06 -0400

    Update step1_extracting_nonMZ_individuals.sh

--- post-imputation/step1_extracting_nonMZ_individuals.sh
+++ post-imputation/step1_extracting_nonMZ_individuals.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -euo pipefail
+
 chr=$1
 path=/local/chib/toconnor_grp/victor/IHC_projects/ABCD/Imputation_TOPMed_20250626
 bcftools=/local/chib/toconnor_grp/victor/softwares/bcftools-1.19/bin/bcftools
 plink2=/local/chib/toconnor_grp/victor/softwares/plink2
 imputedvcf=$path/chr${chr}/chr${chr}.dose.vcf.gz
 tempvcf=$path/chr${chr}/20250626_r2_0.8_updated_10204nonMZ_chr${chr}.vcf.gz
-tempvcf=$path/chr${chr}/20250626_r2_0.8_updated_2141unrelated_chr${chr}.vcf.gz
+tempvcf2=$path/chr${chr}/20250626_r2_0.8_updated_2141unrelated_chr${chr}.vcf.gz
 baseline_assoc=$path/chr${chr}/20250626_r2_0.8_Baseline_10204nonMZ_chr${chr}.assoc.vcf.gz
 baseline_hsq=$path/chr${chr}/20250626_r2_0.8_Baseline_8802Unrelated_chr${chr}.hsq
 trajgwasinput=$path/chr${chr}/20250626_r2_0.8_3TP_2141_unrelated_chr${chr}.assoc.vcf.gz
@@ -16,7 +18,7 @@ threads=4
 
 ## TOPMed Imputation results have multiallelic variants splitted as biallelic
 
-$bcftools view -i 'R2>.8' $imputedvcf -Ou --threads ${threads} | $bcftools view -S $list $imputedvcf -Ou --threads ${threads} \
+$bcftools view -i 'R2>.8' $imputedvcf -Ou --threads ${threads} | $bcftools view -S $list -Ou --threads ${threads} \
 | $bcftools norm -m +any -Ou --threads ${threads} | $bcftools view -m2 -M2 -Ou --threads ${threads} | $bcftools +fill-tags -Oz -o $tempvcf -- -t AC,MAF
 
 $bcftools index $tempvcf
@@ -33,4 +35,7 @@ $plink2 --vcf $tempvcf --remove $related --maf 0.01 --make-bed --set-all-var-ids
 $bcftools view -S $unrelated3TP $tempvcf -Ou --threads ${threads} | $bcftools +fill-tags -Oz -o $tempvcf2 -- -t AC,MAF
 $bcftools index $tempvcf2
 $bcftools view -i 'AC>20' $tempvcf2 --threads ${threads} -Oz -o ${trajgwasinput} ; $bcftools index ${trajgwasinput}
+
+#  Cleanup (only if everything above succeeded) ===
+rm -f "$tempvcf" "$tempvcf.csi" "$tempvcf2" "$tempvcf2.csi"
 
